0adb3ffb2ebfaf1f6128c2be64d2119c2b6d8199
Ian-72 <ragilmurdiantoro@gmail.com>
2022-05-10 13:30:08 +0700

    update route

--- src/server.js
+++ src/server.js
@@ -14,10 +14,39 @@ const Hapi = require('@hapi/hapi');
 
   // Menambahkan route / dengan method GET
   await server.route([
+    // route root
     {
       method: 'GET',
       path: '/',
       handler: async (request, h) => 'Welcome to Obatin API!', // response ketika user masuk ke /
+    },
+    // route register
+    {
+      method: 'POST',
+      path: '/users',
+      handler: async (request, h) => ' POST /users',
+    },
+    // route auth
+    {
+      method: 'POST', // menambahkan token di database
+      path: '/auth',
+      handler: async (request, h) => 'Welcome to Obatin API!',
+    },
+    {
+      method: 'PUT', // mengedit token yang ada di database
+      path: '/auth',
+      handler: async (request, h) => 'Welcome to Obatin API!',
+    },
+    {
+      method: 'DELETE',
+      path: '/auth', // mengahpus token yang ada di ddatabase
+      handler: async (request, h) => 'Welcome to Obatin API!',
+    },
+    // route talk to bot
+    {
+      method: 'POST',
+      path: '/talk',
+      handler: async (request, h) => 'Welcome to Obatin API!',
     },
   ]);
 
