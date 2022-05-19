66920f4c417c871fcd4c3c4240574b7b4ca44ebf
Ian-72 <ragilmurdiantoro@gmail.com>
2022-05-19 11:33:01 +0700

    change description

--- src/api/talk/documentation.js
+++ src/api/talk/documentation.js
@@ -2,8 +2,9 @@ const Joi = require('joi');
 //
 const doc = {
   postTalkDoc: {
+    auth: 'obatin_api_jwt',
     description: 'Talk to BOT',
-    notes: 'This is endpoint where you can talk with Obatin BOT. Payload only contain message field, after you fill message field and sending request to server, server will response 200 and data field is response from BOT.',
+    notes: 'This is endpoint where you can talk with Obatin BOT. Payload only contains message field, after you fill message field and send request to server, server will response 200 and data contains response from BOT.',
     tags: ['api', 'talk'],
     validate: {
       payload: Joi.object({
