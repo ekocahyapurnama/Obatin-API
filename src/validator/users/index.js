8b7bd8b41c2e1d31ccd01239645108c4c0a7ac72
Ian-72 <ragilmurdiantoro@gmail.com>
2022-06-04 21:57:04 +0700

    create validateUserQuery function

--- src/validator/users/index.js
+++ src/validator/users/index.js
@@ -1,9 +1,15 @@
 const InvariantError = require('../../exceptions/InvariantError');
-const userPayloadSchema = require('./schema');
+const { userPayloadSchema, userQuerySchema } = require('./schema');
 
 const UserValidator = {
   validateUserPayload: (payload) => {
     const validationResult = userPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+  validateUserQuery: (query) => {
+    const validationResult = userQuerySchema.validate(query);
     if (validationResult.error) {
       throw new InvariantError(validationResult.error.message);
     }
